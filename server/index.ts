32a93446525a94e8f8cf660f2bbc5c3d696ffd9a
Akhil-L <your-github-alakshup@gitam.in>
2026-05-27 00:31:32 +0530

    Fix

--- server/index.ts
+++ server/index.ts
@@ -29,6 +29,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     httpOnly: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
   }
